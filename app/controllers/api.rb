1c8820b1bdbda99b9393b1c136a010b62c863ac7
Meck <yesmeck@gmail.com>
2013-05-23 19:29:46 +0800

    重新生成 access_token

--- app/controllers/api.rb
+++ app/controllers/api.rb
@@ -4,4 +4,11 @@ DoubanFmHotkeyServer::App.controllers :api do
     render 'api/index'
   end
 
+  post :regenerate, map: '/api/regenerate' do
+    current_user.api_key.destroy
+    current_user.api_key = ApiKey.new
+    current_user.save
+    redirect_to url_for(:api, :index)
+  end
+
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,36 +1,38 @@
 $(function() {
 
-  var client = new Faye.Client('/faye');
+  if ($('#song-info')) {
+    var client = new Faye.Client('/faye');
 
-  client.subscribe('/info/' + ACCESS_TOKEN, function(message) {
-    console.log(message);
-    $pause_button = $('button.pause');
-    $love_button = $('button.love');
-    if (message.radio.is_paused == 'true') {
-      $pause_button.text('播放');
-    } else {
-      $pause_button.text('暂停');
-    }
-    if (message.radio.selected_like == 'true') {
-      $love_button.text('取消喜欢');
-    } else {
-      $love_button.text('喜欢');
-    }
-    $('#cover').html('<img src="' + message.song.coverUrl + '" />');
-    $('#channel').html('频道: ' + message.song.channelName);
-    $('#artist').html('歌手: ' + message.song.artistName);
-    $('#song-name').html('歌名: ' + message.song.songName);
-  });
+    client.subscribe('/info/' + ACCESS_TOKEN, function(message) {
+      console.log(message);
+      $pause_button = $('button.pause');
+      $love_button = $('button.love');
+      if (message.radio.is_paused == 'true') {
+        $pause_button.text('播放');
+      } else {
+        $pause_button.text('暂停');
+      }
+      if (message.radio.selected_like == 'true') {
+        $love_button.text('取消喜欢');
+      } else {
+        $love_button.text('喜欢');
+      }
+      $('#cover').html('<img src="' + message.song.coverUrl + '" />');
+      $('#channel').html('频道: ' + message.song.channelName);
+      $('#artist').html('歌手: ' + message.song.artistName);
+      $('#song-name').html('歌名: ' + message.song.songName);
+    });
 
-  $buttons = $('button');
-  $buttons.click(function() {
-    $this = $(this);
-    var cmd = $this.data('cmd')
-    $.get('/douban/' + cmd + '?access_token=' + ACCESS_TOKEN)
-  });
+    $buttons = $('button');
+    $buttons.click(function() {
+      $this = $(this);
+      var cmd = $this.data('cmd')
+      $.get('/douban/' + cmd + '?access_token=' + ACCESS_TOKEN)
+    });
 
-  setTimeout(function() {
-    $.get('/douban/get_info?access_token=' + ACCESS_TOKEN);
-  }, 2000);
+    setTimeout(function() {
+      $.get('/douban/get_info?access_token=' + ACCESS_TOKEN);
+    }, 2000);
+  }
 
 });
