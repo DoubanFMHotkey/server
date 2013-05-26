865186f4745fdf9727f236b20928c994a555f2de
Meck <yesmeck@gmail.com>
2013-05-27 05:40:55 +0800

    繼續修

--- app/helpers/application.rb
+++ app/helpers/application.rb
@@ -2,10 +2,10 @@ require "#{PADRINO_ROOT}/config/faye_token"
 
 DoubanFmHotkeyServer::App.helpers do
 
-  def broadcast(channel, message)
-    message.merge!(ext: { auth_token: FAYE_TOKEN })
-    @client ||= Faye::Client.new(Settings.faye_url)
-    @client.publish(channel, message)
+  def broadcast(channel, data)
+    message = {:channel => channel, :data => data, :ext => {:auth_token => FAYE_TOKEN}}
+    uri = URI.parse(Settings.faye_url)
+    Net::HTTP.post_form(uri, :message => message.to_json)
   end
 
   def current_user
