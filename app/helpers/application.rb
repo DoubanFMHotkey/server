ec7ef4d8ae080712972e7300e3c962fef20fc5b6
Meck <yesmeck@gmail.com>
2013-09-23 00:43:33 +0800

    可以通过 access_token 获取 user

--- app/helpers/application.rb
+++ app/helpers/application.rb
@@ -10,7 +10,14 @@ DoubanFmHotkeyServer::App.helpers do
   end
 
   def current_user
-    @current_user ||= User.find(session[:user]) if session[:user]
+    return @current_user if @current_user
+    if session[:user]
+      @current_user = User.find(session[:user])
+    elsif params[:access_token]
+      api_key = ApiKey.find_by_access_token(params[:access_token])
+      return nil if api_key.nil?
+      @current_user = api_key.user
+    end
   end
 
   def logged_in?
